2f62b45552fa6c6ae5fb8df0dbbb09f513240a9c
yosef-agil <yosefagil8@agil.com>
2022-08-10 18:17:31 +0700

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Yosef Agil on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
